2f2d90850763d90e01ee9690ecbb80257ccca6e0
Justin Wu <juns3700@gmail.com>
2022-06-27 21:39:09 +0800

    add RWD

--- rate-star/tailwind.config.js
+++ rate-star/tailwind.config.js
@@ -1,6 +1,12 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    screens: {
+      'xs': {'max': '390px'},
+      ...defaultTheme.screens,
+    },
     extend: {},
   },
   plugins: [],
