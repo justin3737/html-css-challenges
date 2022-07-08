05787c15eefb7e61652c5a54b861928d87317f3b
Justin Wu <juns3700@gmail.com>
2022-07-08 16:38:35 +0800

    改用inpit radio取值

--- rate-star/src/js/index.js
+++ rate-star/src/js/index.js
@@ -1,20 +1,21 @@
-let _rate = 0;
+//let _rate = 0;
 const rate_btns = document.querySelectorAll('.rate_btn');
 const submit_btn = document.getElementById('submit');
 const form_view =  document.getElementById('form_view');
 const result_view =  document.getElementById('result_view');
 const rate_text =  document.getElementById('rate_text');
 
-for(let i =0; i < rate_btns.length; i++ ){
-  let self = rate_btns[i];
-  self.addEventListener('click', (elm) => {
-    _rate = parseInt(elm.target.innerHTML, 10)
-    toogleClass(_rate)
-  })
-}
+// for(let i =0; i < rate_btns.length; i++ ){
+//   let self = rate_btns[i];
+//   self.addEventListener('click', (elm) => {
+//     _rate = parseInt(elm.target.innerHTML, 10)
+//     toogleClass(_rate)
+//   })
+// }
 
 submit_btn.addEventListener('click', ()=> {
-  rate_text.innerHTML = `You selected ${_rate} out of 5`;
+  const _val = document.querySelector('input[name="score"]:checked').value;
+  rate_text.innerHTML = `You selected ${_val} out of 5`;
   form_view.classList.add('hidden');
   form_view.classList.remove('flex');
   result_view.classList.add('flex');
