5e2cd760d044ccab64e9cfacfc303d837ee1a6ce
Justin Wu <juns3700@gmail.com>
2022-08-30 11:56:21 +0800

    finish desktop

--- product-card/tailwind.config.js
+++ product-card/tailwind.config.js
@@ -3,7 +3,16 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'green': 'hsl(158, 36%, 37%)',
+        'green-darker': 'hsl(158, 36%, 17%)'
+      },
+      backgroundImage: {
+        'desktop': "url('../img/image-product-desktop.jpg')",
+        'mobile': "url('../img/image-product-mobile.jpg')"
+      }
+    },
   },
   plugins: [],
 }
