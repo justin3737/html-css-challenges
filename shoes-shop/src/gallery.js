3b67a62aba8e5b5397554dbdbab0dfc15e454941
Justin Wu <juns3700@gmail.com>
2022-10-16 13:46:39 +0800

    adjust shoes-page style

--- shoes-shop/src/gallery.js
+++ shoes-shop/src/gallery.js
@@ -1,6 +1,6 @@
 const btns = document.getElementsByClassName('tmb-btn')
 const imgWraps = document.getElementsByClassName('img-wrap')
-const imageW = 450;
+const imageW = 448;
 
 for(let i=0; i<btns.length; i++) {
   btns[i].addEventListener('click', ()=> {
@@ -13,8 +13,8 @@ function doMove(newIdx) {
     let posX = (newIdx - i) * imageW * -1;
     imgWraps[i].style.transform = `translateX(${posX}px)`
     btns[i].classList.add('opacity-50');
-    btns[i].classList.remove('border-4', 'rounded-xl', 'border-orange-400');
+    btns[i].classList.remove('border-2', 'rounded-xl', 'border-orange-400');
   }
   btns[newIdx].classList.remove('opacity-50');
-  btns[newIdx].classList.add('border-4', 'rounded-xl', 'border-orange-400');
+  btns[newIdx].classList.add('border-2', 'rounded-xl', 'border-orange-400');
 }
--- shoes-shop/tailwind.config.js
+++ shoes-shop/tailwind.config.js
@@ -2,22 +2,5 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
   content: ["./src/**/*.{html,js}"],
-  theme: {
-    screens: {
-      'ssm': {
-        'max': '375px'
-      }
-    },
-    extend: {
-      colors: {
-        'green': 'hsl(158, 36%, 37%)',
-        'green-darker': 'hsl(158, 36%, 17%)'
-      },
-      backgroundImage: {
-        'desktop': "url('../img/image-product-desktop.jpg')",
-        'mobile': "url('../img/image-product-mobile.jpg')"
-      }
-    },
-  },
   plugins: [],
 }
